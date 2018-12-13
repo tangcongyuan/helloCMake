e6292fb275fe0f8a033160eb0c8d9b0aae1511ff
Congyuan Tang <cytang@iluvatar.ai>
2018-12-12 18:56:55 -0800

    Completed one deck of cards, 54 cards.

--- src/Deck.cpp
+++ src/Deck.cpp
@@ -6,4 +6,6 @@ Deck::Deck(int num) : _num_of_cards(num) {
     for(const Card::Suit& suit : this->_suits)
         for(const Card::Value& value : this->_values)
             this->_cards.emplace_back(suit, value);
+    this->_cards.emplace_back(Card::Suit::Diamonds, Card::Value::Small_Joker);
+    this->_cards.emplace_back(Card::Suit::Diamonds, Card::Value::Big_Joker);
 }
